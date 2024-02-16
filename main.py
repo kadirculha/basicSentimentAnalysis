e174e558f6029c975d20c05933c482215e0cee5f
Abdulkadir <84346396+kadirculha@users.noreply.github.com>
2024-02-16 16:13:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import string
+from collections import Counter
+
+import matplotlib.pyplot as plt
+
+# reading text file
+text = open("read.txt", encoding="utf-8").read()
+
+# converting to lowercase
+lower_case = text.lower()
+
+# Removing punctuations
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+# splitting text into words
+tokenized_words = cleaned_text.split()
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+final_words = []
+for tokenized_word in tokenized_words:
+    if tokenized_word not in stop_words:
+        final_words.append(tokenized_word)
+
+print(tokenized_words)
+print(final_words)
+
+emotion_list = []
+with open("emotions.txt", "r") as file:
+    for line in file:
+        word, emotion = line.replace("\n", "").replace(",", "").replace("'", "").strip().split(":")
+        emotion = emotion.strip()
+        if word in final_words:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()  # x labelllllls
+plt.savefig('graph.png')
+plt.show()
